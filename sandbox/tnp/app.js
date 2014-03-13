04739cf9a643e214e5c3d69fc05affb29c97ff78
Trevor Perrier <orangpples@gmail.com>
2014-03-13 15:24:55 -0700

    merging with fahad

--- sandbox/tnp/app.js
+++ sandbox/tnp/app.js
@@ -118,7 +118,8 @@ DE = DataExplore = function() {
 	}
 	
 	//private vars
-	var DHIS,rawData,orgData,brush_pos;
+	var DHIS,rawData,orgData,brush_pos,colors = d3.scale.category20();
+	
 	/********
 	Public functions
 	*********/
@@ -128,7 +129,7 @@ DE = DataExplore = function() {
 		console.log('JSON Load: ',dhis,analytics);
 		DHIS = G.dhis = dhis, rawData = G.data = processRaw(analytics);
 		orgData = G.orgData = getOrgChildren(DHIS.orgRoot);
-		//orgData = G.orgData = getOrgChildren('PaqugoqjRIj');
+		orgData = G.orgData = getOrgChildren('jmIPBj66vD6');
 		
 		G.chartEle = d3.select("#bar-chart-ele")
 		.attr({
@@ -151,6 +152,7 @@ DE = DataExplore = function() {
 			});
 			
 		pub.make_bar_chart();
+		pub.drill_up();
 	}
 	
 	pub.make_bar_chart = function(){
@@ -161,17 +163,30 @@ DE = DataExplore = function() {
 		dims.y.domain([0,d3.max(orgData,getPE())]);
 		dims.x.domain(sliderDomain());
 	
-		G.chart.selectAll('g').data(orgData).enter().append('g')
+		var bars = G.chart.selectAll('g').data(orgData).enter().append('g')
 			.attr({ 
 				transform:getPE(function(d,i){return 'translate('+i*dims.barWidth + ',0)';})
 				})
-			.append('rect')
+		bars.append('rect')
 			.attr({
 				width:dims.barWidth-1,
 				height:getPE(function(d){return dims.height-dims.y(d)}),
 				y:getPE(function(d){return dims.y(d)}),
-				class:'bar'
-				}).on('click',DE.stack_click);
+				class:'bar',
+				fill:function(d,i){return colors(i)},
+				}).on('click',DE.drill_down);
+		bars.append('g')
+			.attr({
+				class:'bar-label-ele',
+				transform:getPE(function(d){return 'translate(5,'+(dims.y(d)+5)+')'}),
+			})
+			.append('text')
+			.attr({
+				class:'bar-label',
+				transform:'rotate(90)',
+			})
+			.text(function(d){return d.name;});
+			
 				
 		G.chartEle.append('g')
 			.attr({
@@ -189,9 +204,10 @@ DE = DataExplore = function() {
 				var i = Math.floor(d3.mouse(this)[0]/bandWidth);
 				var value = i*bandWidth+bandWidth/2+5;
 				var new_pos = dims.x.domain()[i];
+				console.log('Clicked: ',new_pos,brush_pos);
 				if(new_pos != brush_pos){
-					DE.change_bar_chart(new_pos);
 					brush_pos = new_pos;
+					DE.change_bar_chart();
 					d3.select(this).select('circle')
 						.attr({
 							cx:value,
@@ -238,11 +254,13 @@ DE = DataExplore = function() {
 	pub.change_bar_chart = function(){
 		console.log("Change Grpah: "+brush_pos);
 		var dims = G.dims.chart;
-		
+		console.log(dims.y.domain(),orgData.map(getPE()));
 		dims.y.domain([0,d3.max(orgData,getPE())]);
+		console.log(dims.y.domain(),orgData.map(getPE()));
+		
 		G.chart.selectAll('rect').data(orgData).transition().duration(1000)
 			.attr({
-				height:getPE(function(d){return dims.height-dims.y(d)}),
+				height:getPE(function(d){console.log(d);return dims.height-dims.y(d)}),
 				y:getPE(function(d){return dims.y(d)}),
 			});
 			
@@ -250,38 +268,158 @@ DE = DataExplore = function() {
 		G.chartEle.select('.y.axis').transition().duration(1000).call(G.axis.chart.y);
 	}
 	
-	pub.stack_click = function(data,i){
+	pub.drill_down = function(data,i){
 		console.log('Click: ',data,i);
+		
+		//get proportions for height
 		var parent_part = orgData[i].data[brush_pos];
 		orgData = getOrgChildren(data.id);
 		var children_part = d3.sum(orgData,getPE());
 		var parent_max = G.dims.chart.y.domain()[1];
 		var children_max = parent_max*children_part/parent_part;
 		console.log(parent_part,parent_max,children_part,children_max);
-		//G.dims.chart.y.domain([0,d3.max(data)]);
-		//DE.make_stacked_chart(i);
-		//DE.unstack();
-		//change y domain
 		
-		//DE.switch_stack();
+		G.dims.chart.y.domain([0,children_max]);
+		
+		DE.make_stacked_chart(i);
+		
+		//Set  new axis vars
+		G.dims.chart.y.domain([0,d3.max(orgData,getPE())]);
+		G.dims.chart.barWidth = G.dims.chart.width/G.stacked.selectAll('g')[0].length;
+		
+		DE.unstack();
+		DE.switch_stack();
 	}
 	
 	pub.make_stacked_chart = function(i){
-	var stack = 0, colors = d3.scale.category20();
+		var stack = 0;
 		
-	//add stack
-	G.stacked.selectAll('g').data(orgRoot).enter().append('g')
+		//add stack
+		G.stacked.selectAll('g').data(orgData).enter().append('g')
+			.attr({
+				transform:'translate('+i*G.dims.chart.barWidth + ',0)',
+			})
+			.append('rect')
+			.attr({
+				width:G.dims.chart.barWidth-1,
+				height:getPE(function(d,i){return G.dims.chart.height-G.dims.chart.y(d)}),
+				y:getPE(function(d){var o = stack; stack += d; return G.dims.chart.y(d+o)}),
+				class:'bar',
+				fill:getPE(function(d,i){return colors(i)})
+			});
+	}
+	
+	pub.unstack = function(delay,type,dur){
+		delay = delay || 0, dur = dur || 1000, type = type || 'sin';
+		
+		var t = d3.transition().duration(dur).delay(delay).ease(type);
+		
+		//hid bar chart
+		t.selectAll('.chart g').attr({
+			opacity:0,
+		});
+		
+		//unwrap stack
+		G.stacked.selectAll('g').transition().delay(delay).ease(type).duration(dur)
 		.attr({
-			transform:'translate('+i*G.dims.chart.barWidth + ',0)',
-		})
-		.append('rect')
+			transform:function(d,i){return 'translate('+i*G.dims.chart.barWidth + ',0)';}
+		});
+		
+		G.stacked.selectAll('rect').transition().delay(delay).ease(type).duration(dur)
 		.attr({
 			width:G.dims.chart.barWidth-1,
-			height:getPE(function(d,i){console.log(d,i);return G.dims.chart.height-G.dims.chart.y(d)}),
-			y:getPE(function(d){var o = stack; stack += d; return G.dims.chart.y(d+o)}),
-			class:'bar',
-			fill:getPE(function(d,i){return colors(i)})
+			height:getPE(function(d,i){return G.dims.chart.height-G.dims.chart.y(d)}),
+			y:getPE(function(d){return G.dims.chart.y(d)})
+		});
+	},
+	
+	pub.switch_stack = function(delay){
+		console.log(delay);
+		delay = delay || 1000
+		console.log(delay);
+		var oldStack = G.stacked.attr('class','chart');
+		oldStack.selectAll('rect').on('click',DE.drill_down);;
+		
+		G.stacked = G.chart.attr('class','stack');
+		G.stacked.selectAll('*').remove();
+		
+		G.chart = oldStack;
+		
+		//Add bar labels
+		G.chart.selectAll('g').append('g')
+			.attr({
+				class:'bar-label-ele',
+				transform:getPE(function(d){return 'translate(5,'+(G.dims.chart.y(d)+5)+')'}),
+			})
+			.append('text')
+			.attr({
+				class:'bar-label',
+				transform:'rotate(90)',
+				visibility:'hidden',
+			})
+			.text(function(d){return d.name;}).transition().delay(delay)
+			.attr('visibility','visible');
+	}
+	
+	pub.drill_up = function(){
+		console.log(orgData[0]);
+		var parent = DHIS.organisationUnits[orgData[0].parent];
+		var parentOrgData = getOrgChildren(parent.parent);
+		var idx = 0;
+		parentOrgData.forEach(function(d,i){
+			if(d.id == parent.id){
+				idx = i;
+			}
+		});
+		
+		console.log(parentOrgData[idx]);
+		
+		//get proportions for height
+		var parent_part = parentOrgData[idx].data[brush_pos];
+		var children_total = d3.sum(orgData,getPE());
+		var parent_max = d3.max(parentOrgData,getPE());
+		var children_max = parent_max*children_total/parent_part;
+		console.log(parent_part,parent_max,children_total,children_max);
+		
+		//set new domain
+		G.dims.chart.y.domain([0,children_max]);
+		G.dims.chart.y.barWidth = G.dims.chart.y.width/parentOrgData.length;
+		var stack = 0;
+	
+		//add stack
+		G.chart.selectAll('g.bar-label-ele').remove();
+		G.chart.selectAll('g').data(orgData).transition().duration(1000)
+			.attr({
+				transform:'translate('+idx*G.dims.chart.barWidth + ',0)',
+			}).selectAll('rect')
+			.attr({
+				width:G.dims.chart.barWidth-1,
+				height:getPE(function(d,i){return G.dims.chart.height-G.dims.chart.y(d)}),
+				y:getPE(function(d){var o = stack; stack += d; return G.dims.chart.y(d+o)}),
+				class:'bar',
 			});
+			
+		//fade in new bars and out old bars
+		G.dims.chart.y.domain([0,parent_max]);
+		var bars = G.stacked.selectAll('g').data(parentOrgData).enter().append('g')
+			.attr({
+				transform:getPE(function(d,i){return 'translate('+i*G.dims.chart.barWidth + ',0)'}),
+			});
+		var rect = bars.append('rect').attr({
+			width:G.dims.chart.barWidth-1,
+			fill:getPE(function(d,i){return colors(i)}),
+			height:0,
+			y:G.dims.chart.y(0),
+			class:'bar',
+		});
+		rect.transition().delay(1000).duration(1000)
+			.attr({
+				height:getPE(function(d){return G.dims.chart.height-G.dims.chart.y(d)}),
+				y:getPE(function(d){return G.dims.chart.y(d)}),
+			});
+			
+		orgData = parentOrgData;
+		window.setTimeout(DE.switch_stack.bind(null,1),2000);
 	}
 	
 	pub.play = function(){
